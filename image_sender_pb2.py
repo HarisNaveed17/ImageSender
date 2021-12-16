87c399dd6c8d55db36fe3117ebf36605750ceeb3
HarisNaveed17 <haris.naveed174@gmail.com>
2021-12-16 12:01:37 +0500

    Add batch size field

--- image_sender_pb2.py
+++ image_sender_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x12image_sender.proto\"5\n\x05Image\x12\r\n\x05image\x18\x01 \x01(\x0c\x12\x0e\n\x06height\x18\x02 \x01(\x05\x12\r\n\x05width\x18\x03 \x01(\x05\"\x17\n\x08Response\x12\x0b\n\x03\x61\x63k\x18\x01 \x01(\t2*\n\x0bImageSender\x12\x1b\n\x04Send\x12\x06.Image\x1a\t.Response\"\x00\x62\x06proto3'
+  serialized_pb=b'\n\x12image_sender.proto\"I\n\x05Image\x12\r\n\x05image\x18\x01 \x01(\x0c\x12\x0e\n\x06height\x18\x02 \x01(\x05\x12\r\n\x05width\x18\x03 \x01(\x05\x12\x12\n\nbatch_size\x18\x04 \x01(\x05\"\x17\n\x08Response\x12\x0b\n\x03\x61\x63k\x18\x01 \x01(\t2*\n\x0bImageSender\x12\x1b\n\x04Send\x12\x06.Image\x1a\t.Response\"\x00\x62\x06proto3'
 )
 
 
@@ -54,6 +54,13 @@ _IMAGE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='batch_size', full_name='Image.batch_size', index=3,
+      number=4, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -67,7 +74,7 @@ _IMAGE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=22,
-  serialized_end=75,
+  serialized_end=95,
 )
 
 
@@ -98,8 +105,8 @@ _RESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=77,
-  serialized_end=100,
+  serialized_start=97,
+  serialized_end=120,
 )
 
 DESCRIPTOR.message_types_by_name['Image'] = _IMAGE
@@ -129,8 +136,8 @@ _IMAGESENDER = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=102,
-  serialized_end=144,
+  serialized_start=122,
+  serialized_end=164,
   methods=[
   _descriptor.MethodDescriptor(
     name='Send',
