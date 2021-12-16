de03fefee51b38445145eeab314ef1d15f38eabe
HarisNaveed17 <haris.naveed174@gmail.com>
2021-12-16 10:36:22 +0500

    close channel

--- image_client.py
+++ image_client.py
@@ -3,15 +3,20 @@ import image_sender_pb2
 import image_sender_pb2_grpc
 import numpy as np
 import base64
+import os
 
 
 MAX_MESSAGE_LENGTH = 104857600
+# if os.environ.get('https_proxy'):
+#     del os.environ['https_proxy']
+# if os.environ.get('http_proxy'):
+#     del os.environ['http_proxy']
 
 
 def run():
 
     # with grpc.insecure_channel('127.0.0.1:5005', options=(('grpc.enable_http_proxy', 0),)) as channel:
-    channel = grpc.insecure_channel('127.0.0.1:5005', options=[('grpc.enable_http_proxy', 0),
+    channel = grpc.insecure_channel('localhost:5005', options=[('grpc.enable_http_proxy', 0),
                                                                ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH), ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH), ],)
     stub = image_sender_pb2_grpc.ImageSenderStub(channel)
 
@@ -23,7 +28,6 @@ def run():
         image_req = image_sender_pb2.Image(image=data, height=h, width=w)
         response = stub.Send(image_req)
         print(response)
-
     channel.close()
 
 
